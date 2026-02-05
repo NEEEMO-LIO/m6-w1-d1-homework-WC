8d487890066c4c3aeebcd86fb7f9e5e63784beca
NEEEMO-LIO <liuedwardnemo@gmail.com>
2026-02-04 22:12:12 -0800

    upload

---
+++ node-mongo/app/models/inventory.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const InventorySchema = new mongoose.Schema({
+  prodname: String,
+  qty: Number,
+  price: Number,
+  status: String,
+});
+
+module.exports = mongoose.model("Inventory", InventorySchema);
+
+
+
